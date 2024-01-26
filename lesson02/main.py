70951eb0ef15e1a7b00ae5850eaf5e0d47b94e51
teacher <fmztd@pdatu.edu.ua>
2024-01-26 13:38:16 +0200

    les02-3

--- lesson02/main.py
+++ lesson02/main.py
@@ -1,9 +1,16 @@
-print('\n--- Start ---\n')
+import os
+os.system('clear')
+
+print('--- Start ---\n')
 print('Variables')
 
 number = 12
-string = 'Name'
+string = ' -string-  '
 fracrion = 45.33
-
 print("Int: %d, Str: %s, Fraction number: %d" %(number, string, fracrion))
-print('\n--- End ---')
+
+str_number = str(number)
+str_fraction = str(fracrion)
+print('Convert to string: ' + str_number + string + str_fraction)
+
+print('\n--- End ---', end='\n'*4)
