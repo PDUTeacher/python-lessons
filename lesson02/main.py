ca08115440a726d1898f2598f9d3bbbff743f6b6
Teacher <94376645+PDUTeacher@users.noreply.github.com>
2024-01-26 14:03:56 +0200

    les02-7

--- lesson02/main.py
+++ lesson02/main.py
@@ -16,9 +16,9 @@ print('Convert to string: ' + str_number + string + str_fraction)
 x = 10
 y = 7
 print('\nx=%d, y=%d' %(x, y))
-
 print('Додавання x+y: ', x + y)
-print('Мноення x*y: ', x * y)
+print('Віднімання x-y: ', x - y)
+print('Множення x*y: ', x * y)
 print('Ділення: x/y', x / y)
 print('Підняття до степеня x^y: ', x ** y)
 print('Цілочисельне ділення x//y: ', x // y)
