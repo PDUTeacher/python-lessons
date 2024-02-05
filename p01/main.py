4a61b2bea9ed860d94975cf9b9dc1b5093315b2d
PDUTeacher <fmztd@pdatu.edu.ua>
2024-02-05 16:43:13 +0200

    les21

--- p01/main.py
+++ p01/main.py
@@ -1,10 +1,13 @@
-#https://github.com/PDUTeacher/python-lessons.git
+# https://github.com/PDUTeacher/python-lessons.git
 
 print('---- start ----')
 print('Work')
 print('One', "Two")
 print('One', "Two", sep="\n")
 print("Int: %d, Str: %s, Fraction number: %d" %(12, 'text', 45.55))
+
+# format
+print("Int: %d, Str: %s, Fraction number: %d".format())
 
 print('----- end -----')
 
